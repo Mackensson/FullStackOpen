72c367a6441bb910ed9d07f3e74243de9b75eb3b
Marcus Edlund <96248484+Mackensson@users.noreply.github.com>
2024-03-13 19:06:00 +0100

    exercise 1.5
    progress on part1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -39,26 +39,29 @@ const Total = (props) => {
 
 const App = () => {
   console.log('App Init..')
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React:',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data:',
-      exercises: 7
-    },
-    {
-      name: 'State of a component:',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React:',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data:',
+        exercises: 7
+      },
+      {
+        name: 'State of a component:',
+        exercises: 14
+      }
+    ]
+  }
+  
   return (
     <div>
-      <Header course={course}></Header>
-      <Content parts={parts}></Content>
-      <Total message='Total exercises: ' parts={parts}></Total>
+      <Header course={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Total message='Total exercises: ' parts={course.parts}></Total>
     </div>
   )
 }
