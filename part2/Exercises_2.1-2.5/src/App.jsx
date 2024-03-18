4ab6465aab0fa1551cdb08cf38f53939ae3e2a79
Marcus Edlund <96248484+Mackensson@users.noreply.github.com>
2024-03-18 10:03:46 +0100

    Exercise: 2.5: Separate module step10

--- part2/Exercises_2.1-2.5/src/App.jsx
+++ part2/Exercises_2.1-2.5/src/App.jsx
@@ -1,48 +1,10 @@
-const Course = ({ course, total }) => {
-  console.log("Course:", course, total)
-  return (
-    <div>
-      <Header2 text={course.name} />
-      <Content parts={course.parts} />
-      <p>
-        <strong>Total of {total} exercises</strong>
-      </p>
-    </div>
-  )
-}
+import Course from './Course'
 
 const Header = ({ text }) => {
   console.log("Header:", text)
   return (
     <div>
       <h1>{text}</h1>
-    </div>
-  )
-}
-
-const Header2 = ({text}) => {
-  console.log("Header2", text)
-  return(
-    <div>
-      <h2>{text}</h2>
-    </div>
-  )
-}
-
-const Content = ({ parts }) => {
-  console.log("Content:", parts)
-  return parts.map((part) => (
-    <Part key={part.id} name={part.name} amount={part.exercises} />
-  ))
-}
-
-const Part = ({ name, amount }) => {
-  console.log("Part:", name, amount)
-  return (
-    <div>
-      <p>
-        {name} {amount}
-      </p>
     </div>
   )
 }
@@ -116,4 +78,4 @@ const App = () => {
   )
 }
 
-export default App;
+export default App
