a7a341e5cfb8e1f259c75e11f554eec492071c87
Marcus Edlund <96248484+Mackensson@users.noreply.github.com>
2024-03-24 21:20:08 +0100

    Exercise 2.6: The Phonebook Step 1

--- part2/Exercises_2.6-2.10/src/App.jsx
+++ part2/Exercises_2.6-2.10/src/App.jsx
@@ -6,19 +6,35 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setPersons([...persons, { name: newName }])
+    setNewName('')
+  }
+
+  const inputChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
+      <div>debug: {newName}</div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={inputChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        <ul>
+          {persons.map((person, index) => 
+            <li key={index}>{person.name}</li>)}
+        </ul>
+      </div>
     </div>
   )
 }
