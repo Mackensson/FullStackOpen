a2812b34c2c37ed8bb3b0198511ee5720a317e3d
Marcus Edlund <96248484+Mackensson@users.noreply.github.com>
2024-04-27 08:15:30 +0200

    Phonebook step 11.5
    Improved on the notification message.
    It will now also display warnings.

--- part2/The-Phonebook/src/App.jsx
+++ part2/The-Phonebook/src/App.jsx
@@ -19,81 +19,6 @@ const App = () => {
     });
   }, []);
 
-  const filteredPersons = persons.filter((person) =>
-    person.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    // If no data was provided.
-    if (!newName || !newNumber) {
-      console.log("No data was provided.");
-      window.alert("Please enter both name and number");
-      return;
-    }
-
-    // If user tries to add duplicate data.
-    if (
-      persons.some((p) => p.name.toLowerCase() === newName.toLowerCase()) &&
-      persons.some((p) => p.number === newNumber)
-    ) {
-      console.log("Duplicate data was found.");
-      window.alert(
-        `${newName} with number ${newNumber} is already added to the phonebook`
-      );
-      return;
-    }
-
-    // If user wants to update a number of a person.
-    if (persons.some((p) => p.name.toLowerCase() === newName.toLowerCase())) {
-      if (
-        window.confirm(
-          `${newName} is already added to the phonebook, replace the old number with a new one?`
-        )
-      ) {
-        const person = persons.find((p) => p.name.toLowerCase() === newName.toLowerCase());
-        const updatedPerson = {
-          id: person.id,
-          name: newName,
-          number: newNumber,
-        };
-        personService.update(person.id, updatedPerson).then((jsonData) => {
-          console.log(jsonData, "Updated");
-          const updatedPersons = persons.filter((p) => p.id !== person.id);
-          setPersons([...updatedPersons, jsonData]);
-          setNewName("");
-          setNewNumber("");
-          setMessage(`Updated ${updatedPerson.name}`)
-        });
-      }
-    }
-    // Creating new entry.
-    else {
-      const personObject = {
-        name: newName,
-        number: newNumber,
-      };
-      personService.create(personObject).then((jsonData) => {
-        console.log(jsonData, "Added");
-        setPersons([...persons, jsonData]);
-      });
-      setNewName("");
-      setNewNumber("");
-      setMessage(`Added ${personObject.name}`);
-    }
-  };
-
-  const onDelete = (id, name) => {
-    if (window.confirm(`Delete ${name}?`)) {
-      personService.remove(id).then((jsonData) => {
-        console.log(jsonData, "Removed");
-        const updatedPersons = persons.filter((p) => p.id !== id);
-        setPersons(updatedPersons);
-      });
-    }
-  };
-
   const inputChangeName = (event) => {
     setNewName(event.target.value);
   };
@@ -106,10 +31,91 @@ const App = () => {
     setFilter(event.target.value);
   };
 
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // If no data was provided.
+    if (!newName || !newNumber) {
+      console.log("No data was provided.");
+      setMessage("Warning: Please enter both name and number");
+      return;
+    }
+
+    // If user tries to add duplicate data.
+    if (
+      persons.some((p) => p.name.toLowerCase() === newName.toLowerCase()) &&
+      persons.some((p) => p.number === newNumber)
+    ) {
+      console.log("Duplicate data was found.");
+      setMessage(
+        `Warning: ${newName} with number ${newNumber} is already added to the phonebook`
+      );
+      return;
+    }
+
+    // If user wants to update a number of a person.
+    if (persons.some((p) => p.name.toLowerCase() === newName.toLowerCase())) {
+      if (
+        window.confirm(
+          `${newName} is already added to the phonebook, replace the old number with a new one?`
+        )
+      ) {
+        const person = persons.find(
+          (p) => p.name.toLowerCase() === newName.toLowerCase()
+        );
+        const updatedPerson = {
+          id: person.id,
+          name: newName,
+          number: newNumber,
+        };
+        personService.update(person.id, updatedPerson).then((jsonData) => {
+          console.log(jsonData, "Updated");
+          const updatedPersons = persons.filter((p) => p.id !== person.id);
+          setPersons([...updatedPersons, jsonData]);
+        });
+        setMessage(`Success: Updated ${newName} with new number ${newNumber}`);
+        setNewName("");
+        setNewNumber("");
+      }
+    }
+    // Creating new entry.
+    else {
+      const personObject = {
+        name: newName,
+        number: newNumber,
+      };
+      personService.create(personObject).then((jsonData) => {
+        console.log(jsonData, "Added");
+        setPersons([...persons, jsonData]);
+      });
+      setMessage(`Success: Added ${newName} with number ${newNumber}`);
+      setNewName("");
+      setNewNumber("");
+    }
+  };
+
+  const onDelete = (id, name) => {
+    if (window.confirm(`Delete ${name}?`)) {
+      personService.remove(id).then((jsonData) => {
+        console.log(jsonData, "Removed");
+        const updatedPersons = persons.filter((p) => p.id !== id);
+        setPersons(updatedPersons);
+        setMessage(`Success:${name} was removed from the phonebook`);
+      });
+    }
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <Notification message={notification} setMessage={setMessage}></Notification>
+      <Notification
+        message={notification}
+        setMessage={setMessage}
+      ></Notification>
       <Filter
         text="Filter shown with: "
         inputValue={filter}
--- part2/The-Phonebook/src/Notification.jsx
+++ part2/The-Phonebook/src/Notification.jsx
@@ -2,37 +2,52 @@ import { useEffect, useState } from "react";
 
 const Notification = ({ message, setMessage }) => {
 
-    const [isVisable, setVisable] = useState(false);
-    const defualtStyle = {
-        background: 'lightgrey',
-        fontSize: 20,
-        borderStyle: 'solid',
-        borderRadius: 5,
-        padding: 10,
-        marginBottom: 10,
-        color: 'green'
-    };
-
-    useEffect(() => {
-        if(message){
-            setVisable(true)
-            const timer = setTimeout(() => {
-                setVisable(false);
-                setMessage("");
-            }, 5000);
-            return () => clearTimeout(timer);
-        }
-    })
-
-    if(!isVisable) {
-        return null;
+  useEffect(() => {
+    if (message) {
+      setVisible(true);
+      const timer = setTimeout(() => {
+        setVisible(false);
+        setMessage("");
+      }, 4000);
+      return () => clearTimeout(timer);
     }
+  });
 
-    return (
-        <div style={defualtStyle}>
-            {message}
-        </div>
-    )
-}
+  const [isVisible, setVisible] = useState(false);
+
+  let backgroundColor = "";
+  let parts = message.split(":")
+
+  // Set styles based on user action.
+  switch (parts[0]) {
+    case "Success":
+      backgroundColor = "lightgreen";
+      break;
+    case "Warning":
+      backgroundColor = "yellow";
+      break;
+    case "Error":
+      backgroundColor = "lightcoral";
+      break;
+    default:
+      backgroundColor = "lightgrey";
+  }
+
+  const defaultStyle = {
+    background: backgroundColor,
+    color: "black",
+    fontSize: 20,
+    borderStyle: "solid",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  };
+
+  if (!isVisible) {
+    return null;
+  }
+
+  return <div style={defaultStyle}>{parts[1]}</div>;
+};
 
 export default Notification;
